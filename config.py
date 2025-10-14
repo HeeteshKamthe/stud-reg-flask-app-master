1f9b48415fbf1e9176c7801411325dfaad3bf251
HeeteshKamthe <heeteshkamthe09@gmail.com>
2025-10-14 12:00:58 +0530

    first commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-   class Config:
-       SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://username:password@localhost/student_db'
-       SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://username:password@localhost/student_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
